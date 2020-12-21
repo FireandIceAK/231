7803af97bf429c0a48fb68542eac8ff8d6847e18
LiveOutside <maslihov.v.a@gmail.com>
2020-12-21 20:10:06 +0500

    1

--- app/src/main/java/com/main/a231/MyView.java
+++ app/src/main/java/com/main/a231/MyView.java
@@ -1,10 +1,13 @@
 package com.main.a231;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -16,19 +19,47 @@ public class MyView extends View {
     public MyView(Context context) {
         super(context);
     }
-
-
+    int cellSize = 30;
+    int width;
+    int height;
+    Paint p;
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        width = getWidth();
+        height = getHeight();
+        drawGrid(canvas);
 
-        int a = canvas.getWidth();
-        int b = canvas.getHeight();
-
-        Paint paint = new Paint();
-        paint.setStrokeWidth(10);
-        paint.setColor(Color.RED);
-        canvas.drawLine(0, 0, 100, 200, paint);
     }
-}
+    public void drawGrid(Canvas canvas) {
+        Paint paint = new Paint();
+        p = new Paint();
+        for (int i = 0; i < width / cellSize; i++) {
+            int x = i * cellSize;
+            canvas.drawLine(x, 0, x, height, paint);
+            paint.setColor(Color.parseColor("black"));
+        }
+        for (int i = 0; i < height / cellSize; i++) {
+            int y = i * cellSize;
+            canvas.drawLine(0, y, width, y, paint);
+        }
+    }
+
+        public void drawPoint (Canvas canvas){
+            Paint paint;
+            canvas.drawPoint(50, 60, p);
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        @Override
+        public boolean onTouchEvent (MotionEvent event){
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                Log.i("onTouch", event.getAction() + "");
+            }
+
+
+            return true;
+        }
+    }
